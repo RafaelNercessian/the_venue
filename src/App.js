1ec8cc762b79d45d7887dfb05f8f882f8cb1ae27
RafaelNercessian <rafanercessian@gmail.com>
2019-09-26 11:14:57 -0300

    Adding highlights

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './resources/styles.css';
 import './components/header_footer/Header'
 import Header from "./components/header_footer/Header";
 import Featured from './components/featured/index'
-
+import VenueNfo from "./components/venueNfo";
+import Description from "./components/highlights/Description";
 function App() {
   return (
-    <div className="App" style={{height: "1500px", background: 'red' }}>
+    <div className="App">
         <Header/>
         <Featured/>
+        <VenueNfo/>
+        <Description/>
     </div>
   );
 }
